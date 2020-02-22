885909ed9af558388d0dfc2660e918aaf051d67d
Morihalla <ketelsdieter@hotmail.be>
2020-02-22 02:12:36 +0100

    first CRUD-operations created

--- src/main/java/com/example/demo/controller/CollectorRepository.java
+++ src/main/java/com/example/demo/controller/CollectorRepository.java
@@ -4,6 +4,15 @@ import com.example.demo.model.Collector;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CollectorRepository extends CrudRepository<Collector, String> {
-     }
+
+     List<Collector> findAll();
+
+     Collector save (Collector collector);
+
+     @Override
+     void delete(Collector collector);
+}
--- src/main/java/com/example/demo/controller/ContentRepository.java
+++ src/main/java/com/example/demo/controller/ContentRepository.java
@@ -4,6 +4,15 @@ import com.example.demo.model.ComicContent;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContentRepository extends CrudRepository <ComicContent, String> {
+
+    List<ComicContent> findAll();
+
+    ComicContent save (ComicContent comicContent);
+
+    @Override
+    void delete(ComicContent comicContent);
 }
