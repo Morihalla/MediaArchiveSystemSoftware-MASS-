a199ddd1b48428f9b2ace8ad5725a98b1f61b05e
morihalla <ketelsdieter@hotmail.be>
2020-03-11 19:22:04 +0100

    search should be working now

--- src/main/java/com/example/demo/controller/CollectorController.java
+++ src/main/java/com/example/demo/controller/CollectorController.java
@@ -2,14 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Collector;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/Collectors")
@@ -26,14 +23,19 @@ public class CollectorController {
         return collectorRepository.findAll();
     }
 
-    @GetMapping(value="/result/{tag}")
+    // A chaque fois que tu cree un MaV tu dois avoir un fichier html correspondant au viewName dans le dossier static
+    // Donc il prend le viewName pour connecter, et puis le value due mapping pour le resultat dans le browser?
+    // C'est ca, le viewName indique le HTML qui va etre charger dans le mapping du browser
+    // Et on met le mapping vers /result (avec le requestmapping du haut, ca combine pour etre la page /Collectors/result)
+    // et avec le parametre tag, ca donne un lien du genre "localhost:8080/collectors/result?tag=thor"
+    @GetMapping(value="/result")
     public ModelAndView Search(@RequestParam(value = "tag", required = false) String tag, HttpServletRequest request, HttpServletResponse response) {
         ModelAndView mav = new ModelAndView("Search");
 
         mav.addObject("tag", tag);
         mav.addObject("Search", collectorRepository.findCollectorsByTitleContaining(tag));
 
-        return Search(tag,request,response);
+        return mav;
     }
 
 }
